d2b434c8605f5cae768b149a0c32a6e0744cdf93
xin.li <xin.li@enflame-tech.com>
2023-06-06 20:35:59 +0800

    update to chapter 9

--- lx/lx/lox/Parser.java
+++ lx/lx/lox/Parser.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import java.util.List;
 
+import java.util.Arrays;
+
 import static lx.lox.TokenType.*;
 
 class Parser {
@@ -51,9 +53,69 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(FOR))
+            return forStatement();
+        if (match(IF))
+            return ifStatement();
         if (match(PRINT))
             return printStatement();
+        if (match(WHILE))
+            return whileStatement();
+        if (match(LEFT_BRACE))
+            return new Stmt.Block(block());
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for'."); // 匹配左括号
+        Stmt initializer;
+        if (match(SEMICOLON)) { // 如果(后直接跟;则没有初始化子句
+            initializer = null;
+        } else if (match(VAR)) { // 否则检测var标记确认是否是变量声明
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement(); // 否则是一个表达式
+        }
+        Expr condition = null;
+        if (!check(SEMICOLON)) { // 如果上一子句后直接跟;则没有条件子句
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after loop condition."); // 条件子句后必须有;
+        Expr increment = null;
+        if (!check(RIGHT_PAREN)) { // 如果上一子句后直接跟)则没有循环子句
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ')' after for clauses."); // 循环子句后必须有)
+        Stmt body = statement(); // 循环体
+        // 解析完成，后续基于已有的节点组装for语句
+        // 增量语句在每一次循环体后执行
+        if (increment != null) {
+            body = new Stmt.Block(
+                    Arrays.asList(
+                            body,
+                            new Stmt.Expression(increment)));
+        }
+        // 获取条件子句和循环体，如果没有条件则设置为true
+        if (condition == null)
+            condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+        // 如果有初始化子式，在最前面添加它
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+        return body;
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatement() {
@@ -72,14 +134,31 @@ class Parser {
         return new Stmt.Var(name, initializer);
     }
 
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+        return new Stmt.While(condition, body);
+    }
+
     private Stmt expressionStatement() {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
     }
 
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
     private Expr assignment() {
-        Expr expr = equality();
+        Expr expr = or();
         if (match(EQUAL)) {
             Token equals = previous();
             Expr value = assignment();
@@ -88,6 +167,26 @@ class Parser {
                 return new Expr.Assign(name, value);
             }
             error(equals, "Invalid assignment target.");
+        }
+        return expr;
+    }
+
+    private Expr or() {
+        Expr expr = and();
+        while (match(OR)) {
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr and() {
+        Expr expr = equality();
+        while (match(AND)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, operator, right);
         }
         return expr;
     }
